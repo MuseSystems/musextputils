85f3dc1490d742767824541069bc6eb73f3901d0
Steven C. Buttgereit <scb@musesystems.com>
2017-10-17 08:05:22 -0700

    Add event hook framework save event functionality
    Here we deal explicitly with the "save" event hooks.  We could have done
    this in a more generic function as to deal with all possible events that
    we might want to hook, but other than save, the other events (i.e.
    close) are much more speculative.  If the need arises we'll revise, but
    this is a good enough start that we can at least prove out the method.

--- client/scripts/museUtilsQt.js
+++ client/scripts/museUtilsQt.js
@@ -42,6 +42,12 @@ if (!MuseUtils.isMuseUtilsJsPolyfillLoaded) {
 //////////////////////////////////////////////////////////////////////////
 
 (function(pPublicApi) {
+    // Internal State for Event Hook Framework
+    var nativeSaveFunction = null;
+    var saveHookParentWidget = null;
+    var preSaveEventFuncs = [];
+    var postSaveEventFuncs = [];
+
     //--------------------------------------------------------------------
     //  "Private" Functional Logic
     //--------------------------------------------------------------------
@@ -152,6 +158,83 @@ if (!MuseUtils.isMuseUtilsJsPolyfillLoaded) {
         }
     };
 
+    var addPreSaveHookFunc = function(pFunc) {
+        preSaveEventFuncs.push(pFunc);
+    };
+
+    var removePreSaveHookFunc = function(pFunc) {
+        preSaveEventFuncs = preSaveEventFuncs.filter(function(targFunc) {
+            return targFunc != pFunc;
+        });
+    };
+
+    var addPostSaveHookFunc = function(pFunc) {
+        postSaveEventFuncs.push(pFunc);
+    };
+
+    var removePostSaveHookFunc = function(pFunc) {
+        postSaveEventFuncs = postSaveEventFuncs.filter(function(targFunc) {
+            return targFunc != pFunc;
+        });
+    };
+
+    var runSaveHookFunctions = function() {
+        // Execute the pre-save hook functions.  If we fail a pre-save hook,
+        for (var i1 = 0; i1 < preSaveEventFuncs.length; i1++) {
+            try {
+                preSaveEventFuncs[i1]();
+            } catch (e) {
+                // If we have pre-save exceptions, we abort the save process.
+                throw new MuseUtils.ApiException(
+                    "musextputils",
+                    "We found an error while executing a 'pre-save' hook function.  We will abort the save that you requested.",
+                    "MuseUtils.runSaveHookFunctions",
+                    { thrownError: e }
+                );
+            }
+        }
+
+        // Run the native function.
+        try {
+            nativeSaveFunction();
+        } catch (e) {
+            throw new MuseUtils.ApiException(
+                "musextputils",
+                "We caught an error trying trying to run the native form's save function.  We abort here as our state is indeterminate.",
+                "MuseUtils.runSaveHookFunctions",
+                { thrownError: e }
+            );
+        }
+
+        // Run the post save hooks.  For these we only display and log the message
+        // since we expect that the native save function executed correctly.
+        for (var i2 = 0; i2 < postSaveEventFuncs.length; i2++) {
+            try {
+                postSaveEventFuncs[i2]();
+            } catch (e) {
+                // If we have pre-save exceptions, we abort the save process.
+                var myError = new MuseUtils.ApiException(
+                    "musextputils",
+                    "We found an error while executing a 'post-save' hook function.  The record you are saving has been saved, though there may be problems, especially with any custom module that also must save information.  Please be sure to double check the record you just saved for accurracy.",
+                    "MuseUtils.runSaveHookFunctions",
+                    {
+                        thrownError: myError,
+                        context: {
+                            preSaveEventFuncs: preSaveEventFuncs,
+                            postSaveEventFuncs: postSaveEventFuncs
+                        }
+                    }
+                );
+
+                // A rare example of a display error in a utility function; we
+                // do this here since we want proper exception logging, but we
+                // don't want to raise a general exception which will blow up
+                // the process.
+                MuseUtils.displayError(myError, saveHookParentWidget);
+            }
+        }
+    };
+
     //--------------------------------------------------------------------
     //  Public Interface -- Functions
     //--------------------------------------------------------------------
@@ -242,6 +325,165 @@ if (!MuseUtils.isMuseUtilsJsPolyfillLoaded) {
         }
 
         return getModeFromXtpEnumId(pEnumId);
+    };
+
+    pPublicApi.initSaveHookFramework = function(pNativeSaveFunc, pParent) {
+        // Capture function parameters for later exception references.
+        var funcParams = {
+            pNativeSaveFunc: pNativeSaveFunc,
+            pParent: pParent
+        };
+
+        if (typeof pNativeSaveFunc !== "function") {
+            throw new MuseUtils.ParameterException(
+                "musextputils",
+                "We require a valid reference to the native form's save function in order to initialize the save hook framework.",
+                "MuseUtils.pPublicApi.initSaveHookFramework",
+                { params: funcParams }
+            );
+        }
+
+        if (MuseUtils.realNull(pParent) === null) {
+            throw new MuseUtils.ParameterException(
+                "musextputils",
+                "We require a valid reference to a suitable parent widget which will serve as the parent to any not-fatal exceptions that might be thrown.",
+                "MuseUtils.pPublicApi.initSaveHookFramework",
+                { params: funcParams }
+            );
+        }
+
+        saveHookParentWidget = pParent;
+        nativeSaveFunction = pNativeSaveFunc;
+    };
+
+    pPublicApi.addPreSaveHookFunc = function(pFunc) {
+        // Capture function parameters for later exception references.
+        var funcParams = {
+            pFunc: pFunc
+        };
+
+        if (typeof pFunc !== "function") {
+            throw new MuseUtils.ParameterException(
+                "musextputils",
+                "We require a valid function to add to the pre-save event hook.",
+                "MuseUtils.pPublicApi.addPreSaveHookFunc",
+                { params: funcParams }
+            );
+        }
+
+        try {
+            addPreSaveHookFunc(pFunc);
+        } catch (e) {
+            throw new MuseUtils.ApiException(
+                "musextputils",
+                "We encountered an error trying to add a pre-save event hook function.",
+                "MuseUtils.pPublicApi.addPreSaveHookFunc",
+                { params: funcParams, thrownError: e }
+            );
+        }
+    };
+
+    pPublicApi.removePreSaveHookFunc = function(pFunc) {
+        // Capture function parameters for later exception references.
+        var funcParams = {
+            pFunc: pFunc
+        };
+
+        if (typeof pFunc !== "function") {
+            throw new MuseUtils.ParameterException(
+                "musextputils",
+                "We require a valid function to remove it from the pre-save event hook.",
+                "MuseUtils.pPublicApi.",
+                { params: funcParams }
+            );
+        }
+
+        try {
+            removePreSaveHookFunc(pFunc);
+        } catch (e) {
+            throw new MuseUtils.ApiException(
+                "musextputils",
+                "We encountered an error trying to remove a pre-save event hook function.",
+                "MuseUtils.pPublicApi.removePreSaveHookFunc",
+                { params: funcParams, thrownError: e }
+            );
+        }
+    };
+
+    pPublicApi.addPostSaveHookFunc = function(pFunc) {
+        // Capture function parameters for later exception references.
+        var funcParams = {
+            pFunc: pFunc
+        };
+
+        if (typeof pFunc === "function") {
+            throw new MuseUtils.ParameterException(
+                "musextputils",
+                "We require a valid function to add it to the post-save event hook.",
+                "MuseUtils.pPublicApi.",
+                { params: funcParams }
+            );
+        }
+
+        try {
+            addPostSaveHookFunc(pFunc);
+        } catch (e) {
+            throw new MuseUtils.ApiException(
+                "musextputils",
+                "We encountered an error trying to add post-save event hook function.",
+                "MuseUtils.pPublicApi.addPostSaveHookFunc",
+                { params: funcParams, thrownError: e }
+            );
+        }
+    };
+
+    pPublicApi.removePostSaveHookFunc = function(pFunc) {
+        // Capture function parameters for later exception references.
+        var funcParams = {
+            pFunc: pFunc
+        };
+
+        if (typeof pFunc !== "function") {
+            throw new MuseUtils.ParameterException(
+                "musextputils",
+                "We require a valid function to remove it from the post-save event hook.",
+                "MuseUtils.pPublicApi.",
+                { params: funcParams }
+            );
+        }
+
+        try {
+            removePostSaveHookFunc(pFunc);
+        } catch (e) {
+            throw new MuseUtils.ApiException(
+                "musextputils",
+                "We encountered an error trying to remove a post-save event hook function.",
+                "MuseUtils.pPublicApi.removePostSaveHookFunc",
+                { params: funcParams, thrownError: e }
+            );
+        }
+    };
+
+    pPublicApi.sProcessSaveFramework = function() {
+        if (typeof nativeSaveFunction !== "function") {
+            throw new MuseUtils.ApiException(
+                "musextputils",
+                "The save hook framework has not been initialized, please call the initSaveHookFramework function from this library before calling sProcessSaveFramework",
+                "MuseUtils.pPublicApi.addPostSaveHookFunc",
+                {}
+            );
+        }
+
+        try {
+            runSaveHookFunctions();
+        } catch (e) {
+            throw new MuseUtils.ApiException(
+                "musextputils",
+                "There was a non-recoverable error while trying to save a record.  We will abort the save process here.",
+                "MuseUtils.pPublicApi.sProcessSaveFramework",
+                { thrownError: e }
+            );
+        }
     };
 
     // Set a flag indicating that this library is loaded.
